0ce21b9f7c239166e42571f92aaf3464d1fc9cb2
pezzu <7243796+pezzu@users.noreply.github.com>
2020-07-26 23:50:15 -0400

    Range iterator, kind of

---
+++ Range.js
@@ -0,0 +1,20 @@
+class Range {
+  constructor(begin = 0, end = Infinity) {
+    this.begin = begin;
+    this.end = end;
+    this.index = begin;
+  }
+
+  next() {
+    if (this.index < this.end) {
+      return {value: this.index++, done: false}
+    } else {
+      return {done: true}
+    }
+  }
+
+  [Symbol.iterator]() {
+    return this;
+    // return new Range(this.begin, this.end)
+  }
+}
